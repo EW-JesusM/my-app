9bf25df873b8c6af09b9caaaf288e312a3280d27
Jesus Melero Palmero <jesusmeleropalmero@gmail.com>
2019-10-22 18:16:05 +0200

    Change test again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
                     Laravel 67
                 </div>
                 <div class="title m-b-md">
-                    Styde
+                    Stydeds
                 </div>
 
                 <div class="links">
